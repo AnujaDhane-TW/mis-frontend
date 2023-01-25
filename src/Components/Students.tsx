1aa3c584d6281fa6f239b474e60cbec005c511a8
AnujaDhane-TW <anuja.dhane@thoughtworks.com>
2023-01-25 17:20:38 +0530

    [Anuja/Mohini]: Add. checkbox and delete selected functionality

--- src/Components/Students.tsx
+++ src/Components/Students.tsx
@@ -6,18 +6,26 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
-import { IconButton, TextField } from '@mui/material';
+import { Checkbox, IconButton, TextField } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from "react-router-dom";
 import React, { useState } from 'react';
+
 import axios from "axios";
+
+
 
 
 export default function Students() {
   const [students, setStudents] = useState<any>([]);
+  //const [checkedStatus, setCheckedStatus]= useState<Array<Boolean>>([]);
+  let myMap = new Map<number, boolean>();
+  const [checked, setChecked] = React.useState();
+
+
   const navigate = useNavigate();
   function handleEdit(student:any) {
     navigate("/edit",{state:{student:student}});
@@ -30,11 +38,35 @@ export default function Students() {
       console.log(resp.data);
       });
   }
+  const handleCheck = (id:number) => {
+    myMap.set(id,!myMap.get(id));
+  
+    console.log(myMap);
+  };
+  const handleDeleteAll =()=>{
+   
+    myMap.forEach((value: boolean, key: number) => {
+     if(value)
+     {
+      axios.delete(`http://localhost:5026/Student/${key}`).then(resp => {
+      console.log(resp.data);
+      });
+     }
+  });
+
+  }
+    
   function getStudents() {
     axios.get('http://localhost:5026/Student').then(resp => {
       setStudents(resp.data);
       });
-      console.log(students);
+      for (let index = 0; index < students.length; index++) {
+       // checkedStatus.push(false);
+       console.log(students[index].id);
+         myMap.set(students[index].id,false);
+       
+      }
+      console.log(myMap);
   }
   
   React.useEffect(() => {
@@ -51,9 +83,7 @@ export default function Students() {
                 <TextField
                 id="search-bar"
                 className="text"
-                /*onInput={(e) => {
-                    setSearchQuery(e.target.value);
-                }}*/
+              
                 label="Enter student Id"
                 variant="outlined"
                 placeholder="Search..."
@@ -68,15 +98,20 @@ export default function Students() {
         <Button onClick = {handleAdd} startIcon={<AddIcon />} >
                 Add Student
         </Button>
-            <IconButton aria-label="delete" size="large">
+            <IconButton aria-label="delete" size="large" onClick= {handleDeleteAll}>
                 <DeleteIcon fontSize="inherit" />
         </IconButton>
        </div>
       </div>
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table" >
         <TableHead>
           <TableRow>
+          {/* <TableCell padding="checkbox">
+            <Checkbox
+                onChange={onSelectAllClick}
+              />
+          </TableCell> */}
             <TableCell>Id</TableCell>
             <TableCell align="center">First Name</TableCell>
             <TableCell align="center">Middle Name</TableCell>
@@ -91,6 +126,11 @@ export default function Students() {
               key={student.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
+              <Checkbox
+                checked={checked}
+                onChange={()=>handleCheck(student.id)}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
               <TableCell component="th" scope="row">
                 {student.id}
               </TableCell>
